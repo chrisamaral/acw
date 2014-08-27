a60fae5d1fb1f5cf53338941f16983ac8e6a159e
Christian Amaral <darthcas@gmail.com>
2014-08-27 13:46:13 -0300

    fix: atualizações do express

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -6,9 +6,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     expressSession = require('express-session'),
-
     RedisStore = require('connect-redis')(expressSession),
-    //XSession = require('../lib/xsession.js')(expressSession),
+
+    _ = require('lodash'),
+    http = require('http'),
 
     sessMaxAge = 1000 * 60 * 60 * 24,
     customAuth,
@@ -17,31 +18,53 @@ var express = require('express'),
 
 etc.sessionStore = new RedisStore();
 etc.express = express();
+
 etc.express.set('json spaces', 2);
 etc.express.use(express.static(pub_dir));
-etc.express.use(bodyParser());
-etc.express.use(cookieParser());
-function dumpSession(header) {
-    return function (req, res, next) {
-        console.log('#################', header, '###############');
-        console.log(req.cookies, req.session, req.user, req.sessionID);
-        next();
-    };
-}
 
-//etc.express.use(dumpSession('pre-express-session'));
+etc.express.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+etc.express.use(bodyParser.json({limit: '5mb', extended: true}));
+
+etc.express.use(cookieParser());
 
 etc.express.use(expressSession({
+    saveUninitialized: true,
+    resave: true,
     secret: 'maiorsegredodomundo',
     key: 'acw.sid',
     cookie: {
         maxAge: sessMaxAge,
         domain: '.' + etc.DOMAIN
     },
-    //store: new XSession()
     store: etc.sessionStore
 }));
-//etc.express.use(dumpSession('post-express-session'));
+
+/*
+ middleware fix for
+ >> https://github.com/strongloop/express/issues/2269
+ until
+ >> https://github.com/admittedly/express/commit/ed6d0e3819a2e7fe6f2052b4e223e57435c4fdc3
+ gets released
+ */
+
+etc.express.use(function (req, res, next) {
+
+    var resSend = res.send;
+
+    res.send = function () {
+        var statusNumber;
+
+        if (_.isNumber(arguments[0])) {
+            statusNumber = arguments[0];
+            return res.status(statusNumber).end(http.STATUS_CODES[statusNumber]);
+        }
+
+        resSend.apply(res, arguments);
+    };
+
+    next();
+});
+
 etc.express.use(connetFlash());
 etc.express.set('view engine', 'jade');
 
